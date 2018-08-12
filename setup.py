f3da2501313d0693acd4dae6539438aa3f119a0e
pf4d <cummings.evan@gmail.com>
2018-08-12 12:43:35 +0200

    trying to get readthedocs to work

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup, Extension
 
-extension = Extension('fenics_mpm', \
-                      ['fenics_mpm/cpp/MPMElasticMaterial.cpp', \
-                       'fenics_mpm/cpp/MPMImpenetrableMaterial.cpp', \
-                       'fenics_mpm/cpp/MPMMaterial.cpp', \
-                       'fenics_mpm/cpp/MPMModel.cpp'], \
-                      include_dirs=['fenics_mpm/cpp/'], \
-                      language="c++")
+#extension = Extension('fenics_mpm', \
+#                      ['fenics_mpm/cpp/MPMElasticMaterial.cpp', \
+#                       'fenics_mpm/cpp/MPMImpenetrableMaterial.cpp', \
+#                       'fenics_mpm/cpp/MPMMaterial.cpp', \
+#                       'fenics_mpm/cpp/MPMModel.cpp'], \
+#                      include_dirs=['fenics_mpm/cpp/'], \
+#                      language="c++")
 
 
 setup(  name        = 'fenics_mpm',
@@ -15,8 +15,8 @@ setup(  name        = 'fenics_mpm',
         author      = 'Evan M. Cummings',
         url         = 'https://github.com/pf4d/fenics_mpm',
         packages    = ['fenics_mpm'],
-        package_dir = {'fenics_mpm' : 'fenics_mpm'},
-        ext_modules = extension)
+        package_dir = {'fenics_mpm' : 'fenics_mpm'})
+        #ext_modules = extension)
 
 
 
